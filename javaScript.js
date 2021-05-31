1fa4720fc7ab6488204d2761c81aa524247077a5
abdelra <>
2021-05-31 14:53:54 +0000

    lab06

--- javaScript.js
+++ javaScript.js
@@ -1,18 +1,105 @@
-// console.log("hello");
-// alert("hello");
-var grade = prompt("what is your grade ?");
+let studentsName = ['Abd Erazek', 'Adham', 'Afnan', 'Hiba', 12, true, ['code101', 'code102', 'code201']];
+
+// console.log(studentsName);
+// console.log(studentsName[1]);
+// console.log(studentsName[3]);
+
+// console.log(studentsName[0], ',' ,studentsName[2])
+
+// console.log(studentsName.length)
+// Start point
+// Comparing between the start point and end point (Count oof the value inside the array)
+// i = i + 1 (increase the i var)
+for (var i = 0; i < studentsName.length; i++) {
+
+  // document.write(studentsName[i]);
+  // if(studentsName[i] == 'Hiba') {
+  //   break;
+  // }
+  // console.log(studentsName[i]);
 
 
-if (grade <= 49){
-  alert("we are sorry , you falid ");
+  // document.write('<img src="https://www.mcicon.com/wp-content/uploads/2021/03/Cat-03.jpg" />')
 }
-else if ( 70 <= grade >=50)
-{
 
-console.log("great ,you passed ");
 
-}
-else {
 
-  document.write("<div>fine doing</div>");
-} 
+
+// logical 
+// && and &
+// || or
+
+// var username = prompt('Please enter your name');
+
+// var userAge = prompt('please enter age');
+
+// if(username == 'Mamoon' && userAge == 23) { 
+//   console.log(true); 
+// }
+
+// if(username == 'Dania' || userAge == 25) { 
+//   console.log(true); 
+// }
+
+// if(username == 'Mamoon' && userAge != 23) { 
+//   console.log(true); 
+// }
+
+// if the user age equa to 30 loop again
+// while(userAge == 30) {
+//   // console.log('inside the whie loop');
+//   // break;
+//   userAge = prompt('please enter your age again!!!!');
+// }
+
+
+
+// while(username != 'Renad' || userAge != 22) {
+//   // console.log('inside the whie loop');
+//   // break;
+//   username = prompt('Please enter your name');
+//   userAge = prompt('please enter age');
+
+// }
+
+// console.log(5 % 2)
+
+// let num1 = 10;
+// let num2 = 130;
+// let num3 = 140;
+// let num4 = 1110;
+
+// let total = num1 + num2 + num3 + num4;
+// console.log(total);
+
+// let num11 = 100;
+// let num22 = 1300;
+// let num33 = 1400;
+// let num44 = 11130;
+
+// let total1 = num11 + num22 + num33 + num44;
+// console.log(total1);
+
+// function sumTotal(n1, n2, n3, n4) {
+//   let total1 = n1 + n2 + n3 + n4;
+//   console.log('the total is',total1);
+// }
+
+// sumTotal(100, 2, 33, 432);
+// sumTotal(1002, 2, 33, 43432);
+// sumTotal(1030, 2, 33, 4352);
+// sumTotal(1500, 2, 332, 4342);
+// sumTotal(1060, 2, 353, 432);
+
+
+// let name1 = prompt('Please enter your name');
+// let familyName = prompt('Please enter your family name');
+
+
+// function printFullname(firstName, lastName) {
+//   console.log('Fullname is', lastName, firstName);
+//   document.write(`Fullname is <b> ${firstName} </b> ${lastName} <br>`);
+// }
+
+// printFullname('Afnan', 'Shihab');
+// printFullname(name1, familyName);
