7eb8e4bc2d7b086961d13e776bd062324cfa8aea
abdelra <>
2021-05-31 14:26:19 +0000

    lab05

---
+++ javaScript.js
@@ -0,0 +1,18 @@
+// console.log("hello");
+// alert("hello");
+var grade = prompt("what is your grade ?");
+
+
+if (grade <= 49){
+  alert("we are sorry , you falid ");
+}
+else if ( 70 <= grade >=50)
+{
+
+console.log("great ,you passed ");
+
+}
+else {
+
+  document.write("<div>fine doing</div>");
+} 
